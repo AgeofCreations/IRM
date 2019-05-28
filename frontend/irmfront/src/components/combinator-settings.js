3e16c009893e02c5d0a687235e7a4718730cdec1
AgeofCreations <tesfnatic123@gmail.com>
2019-05-28 19:52:47 +0500

    fields, buttons and more for combinator

---
+++ frontend/irmfront/src/components/combinator-settings.js
@@ -0,0 +1,60 @@
+import { Collapse, Checkbox, Switch } from 'antd';
+import React from 'react'
+
+const Panel = Collapse.Panel;
+
+
+
+class CombinatorSettings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      combinateDoubles: false,
+      deteleTabs: true,
+      deleteSpaces: true,
+      ignoreEmptyRows: true
+    }
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleSwitches = this.handleSwitches.bind(this);
+  }
+
+  handleCheckboxChange(e) {
+
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name
+      this.setState({
+        [name]: value
+      });
+    }
+
+  handleSwitches(event) {
+    const target = event.target
+    const value = target.id === 'switch' ? target.checked : target.value;
+    const name = target.name
+      this.setState({
+        [name]: value
+      });
+    }
+
+
+  render(){
+    return(
+        <Collapse bordered={true} defaultActiveKey={['']}>
+          <Panel header="Показать настройки" key="1">
+            <div>
+              <div style={{width: '20%'}}>
+                <p>Результат</p>
+                <Checkbox type="checkbox" name="combinateDoubles" checked={this.state.combinateDoubles} onChange={this.handleCheckboxChange} style={{marginLeft: '8px'}}>Пересекать дубли</Checkbox>
+                <Checkbox type="checkbox" name="deteleTabs" checked={this.state.deteleTabs} onChange={this.handleCheckboxChange}>Удалить табуляцию</Checkbox>
+                <Checkbox type="checkbox" name="deleteSpaces" checked={this.state.deleteSpaces} onChange={this.handleCheckboxChange}>Удалить лишние пробелы</Checkbox>
+                <Checkbox type="checkbox" name="ignoreEmptyRows" checked={this.state.ignoreEmptyRows} onChange={this.handleCheckboxChange} style={{marginLeft: '8px'}}>Игнорировать пустые строки</Checkbox>
+              </div>
+            </div>
+          </Panel>
+        </Collapse>
+        )
+    }
+}
+
+export default CombinatorSettings;
